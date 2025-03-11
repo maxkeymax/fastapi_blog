3e96ddcf251290951b8c0da1ec938d00dcbb2be7
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-03-11 19:45:06 +0300

    Edit repository.users.py for async work

--- blog/repository/user.py
+++ blog/repository/user.py
@@ -1,28 +1,28 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException, status
 
 from .. import models, schemas, database
 from ..hashing import Hash
 
-db = database.get_db
-
-def create(request: schemas.User, db: Session):
+async def create(request: schemas.User, db: AsyncSession):
     new_user = models.User(
         name=request.name,
         email=request.email,
         password=Hash.bcrypt(request.password)
     )
     db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+    await db.commit()
+    await db.refresh(new_user)
     return new_user
 
-def show(id: int, db: Session):
-    user = db.query(models.User).filter(models.User.id == id).first()
+async def show(id: int, db: AsyncSession):
+    result = await db.execute(
+        models.User.select().where(models.User.id == id)
+    )
+    user = result.scalars().first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'User {id} not found'
         )
-    
     return user
