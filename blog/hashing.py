3e9685bd7dd7dfb90089c4cb97058383c5347b25
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-03-12 16:05:20 +0300

    Edit hashing.py

--- blog/hashing.py
+++ blog/hashing.py
@@ -1,12 +1,12 @@
 from passlib.context import CryptContext
 
-
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
-class Hash():
-    def bcrypt(password: str):
+class PasswordHasher:
+    @staticmethod
+    def bcrypt(password: str) -> str:
         return pwd_cxt.hash(password)
     
-    def verify(plain_password, hashed_password):
+    @staticmethod
+    def verify(plain_password: str, hashed_password: str) -> bool:
         return pwd_cxt.verify(plain_password, hashed_password)
